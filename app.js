42a54180f18d4cc2461075d28a70760b0698d52f
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-14 21:04:22 +0200

    Iteration#2

---
+++ app.js
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------------
+ ----------------**Iteración #1: Buscar el máximo-------------------------
+ -----------------------------------------------------------------------*/
+
+ const sum = (numberOne, numberTwo) => {
+    return (numberOne > numberTwo) ? numberOne : numberTwo;
+  }
+
+/*-----------------------------------------------------------------------
+----------------**Iteración #2: Buscar la palabra más larga**------------
+-----------------------------------------------------------------------*/
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+const findLongestWord = (list) => {
+  let longestWord = "";
+  list.forEach((element) => {
+    longestWord = element.length > longestWord.length ? element : longestWord;
+  })
+  return longestWord;
+}
+
+console.log(findLongestWord(avengers));
+
